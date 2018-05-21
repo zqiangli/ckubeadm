105a70ff1019d2163190d8666003217ce9ec2692
cherryleo <miaomiao3312763@gmail.com>
2018-05-21 22:08:38 +0800

    更新kubelet安装脚本，支持多版本

--- sh/install-kubelet-kubectl-cni.sh
+++ sh/install-kubelet-kubectl-cni.sh
@@ -1,62 +1,88 @@
-# install ckubeadm dependence
+#!/bin/bash
 # kubernetes version
-KUBERNETES_VERSION="1.9.1"
+KUBERNETES_VERSION="1.9.0"
+ROLE="master"
 
-# install kubelet and create kubelet.service
-function install_kubelet() {
-    # 下载kubelet
-    wget https://fileserver-1253732882.cos.ap-chongqing.myqcloud.com/kubelet-${KUBERNETES_VERSION}-amd64.tgz
-    # 解压
-    tar -zxvf kubelet-1.9.1-amd64.tgz -C /usr/bin/
-    # 创建service
-    cat << EOF > /etc/systemd/system/kubelet.service
-[Unit]
-Description=kubelet: The Kubernetes Node Agent
-Documentation=http://kubernetes.io/docs/
+function install() {
+    yum install -y wget
+    wget https://fileserver-1253732882.cos.ap-chongqing.myqcloud.com/kubernetes-cni/kubernetes-cni-0.6.0-0.x86_64.rpm
+    wget https://fileserver-1253732882.cos.ap-chongqing.myqcloud.com/kubelet/kubelet-${KUBERNETES_VERSION}-0.x86_64.rpm
 
-[Service]
-ExecStart=/usr/bin/kubelet
-Restart=always
-StartLimitInterval=0
-RestartSec=10
-
-[Install]
-WantedBy=multi-user.target
-EOF
-    # 激活service
+    if [ "${ROLE}" = 'master' ]; then
+        wget https://fileserver-1253732882.cos.ap-chongqing.myqcloud.com/kubectl/kubectl-${KUBERNETES_VERSION}-0.x86_64.rpm
+        yum install -y kubectl-${KUBERNETES_VERSION}-0.x86_64.rpm
+    fi
+    yum install -y kubernetes-cni-0.6.0-0.x86_64.rpm kubelet-${KUBERNETES_VERSION}-0.x86_64.rpm
     systemctl enable kubelet.service
 }
 
-# install kubectl
-function install_kubectl() {
-    # 下载kubectl
-    wget https://fileserver-1253732882.cos.ap-chongqing.myqcloud.com/kubectl-${KUBERNETES_VERSION}-amd64.tgz
-    # 解压
-    tar -zxvf kubectl-1.9.1-amd64.tgz -C /usr/local/bin/
-}
+PS3='Select kubernetes version: '
+options=(
+    "1.9.0"
+    "1.9.1"
+    "1.9.2"
+    "1.9.3"
+    "1.9.4"
+    "1.9.5"
+    "1.9.6"
+    "1.9.7"
+)
+select opt in "${options[@]}"
+do
+    case $opt in
+        "1.9.0")
+            KUBERNETES_VERSION="1.9.0"
+            echo $KUBERNETES_VERSION
+            break
+            ;;
+        "1.9.1")
+            KUBERNETES_VERSION="1.9.1"
+            echo $KUBERNETES_VERSION
+            break
+            ;;
+        "1.9.2")
+            KUBERNETES_VERSION="1.9.2"
+            echo $KUBERNETES_VERSION
+            break
+            ;;
+        "1.9.3")
+            KUBERNETES_VERSION="1.9.3"
+            echo $KUBERNETES_VERSION
+            break
+            ;;
+        "1.9.4")
+            KUBERNETES_VERSION="1.9.4"
+            echo $KUBERNETES_VERSION
+            break
+            ;;
+        "1.9.5")
+            KUBERNETES_VERSION="1.9.5"
+            echo $KUBERNETES_VERSION
+            break
+            ;;
+        "1.9.6")
+            KUBERNETES_VERSION="1.9.6"
+            echo $KUBERNETES_VERSION
+            break
+            ;;
+        "1.9.7")
+            KUBERNETES_VERSION="1.9.7"
+            echo $KUBERNETES_VERSION
+            break
+            ;;
+        *) echo invalid option;;
+    esac
+done
 
-# install kubernetes-cni
-function install_cin() {
-    # 下载kubernetes-cni
-    wget https://fileserver-1253732882.cos.ap-chongqing.myqcloud.com/cni-plugins-amd64-v0.6.0.tgz
-    # 解压
-    mkdir -p /opt/cni/bin
-    tar -zxvf cni-plugins-amd64-v0.6.0.tgz -C /opt/cni/bin
-}
-
-# master节点安装kubelet，kubectl，cni
-# node节点安装kubele，cni
 while true; do
     read -p "Is this node master node? (y/n)" yn
     case $yn in
         [Yy]* ) 
-            install_kubelet;
-            install_kubectl;
-            install_cin;
+            install
             break;;
-        [Nn]* ) 
-            install_kubelet;
-            install_cin;
+        [Nn]* )
+            ROLE="node"
+            install
             exit;;
         * ) echo "Please answer yes or no.";;
     esac
