2033817ee4fde787ff0acdea5cd3bd24629c34c1
cherryleo <miaomiao3312763@gmail.com>
2018-05-22 22:18:55 +0800

    更新kubelet安装脚本，支持ubuntu deb包安装

--- sh/install-kubelet-kubectl-cni.sh
+++ sh/install-kubelet-kubectl-cni.sh
@@ -2,18 +2,36 @@
 # kubernetes version
 KUBERNETES_VERSION="1.9.0"
 ROLE="master"
+Ubuntu=$(cat /etc/*elease | grep VERSION_CODENAME)
+CentOS=$(cat /etc/*elease | grep CENTOS_MANTISBT_PROJECT_VERSION)
 
+# entOS7+, Ubuntu16.04
 function install() {
-    yum install -y wget
-    wget https://fileserver-1253732882.cos.ap-chongqing.myqcloud.com/kubernetes-cni/kubernetes-cni-0.6.0-0.x86_64.rpm
-    wget https://fileserver-1253732882.cos.ap-chongqing.myqcloud.com/kubelet/kubelet-${KUBERNETES_VERSION}-0.x86_64.rpm
+    if [[ ${Ubuntu##*=} =~ "xenial" ]]; then
+        apt-get install -y wget
+        wget https://fileserver-1253732882.cos.ap-chongqing.myqcloud.com/kubernetes-cni/kubernetes-cni_0.6.0-00_amd64.deb
+        wget https://fileserver-1253732882.cos.ap-chongqing.myqcloud.com/kubelet/kubelet_${KUBERNETES_VERSION}-00_amd64.deb
 
-    if [ "${ROLE}" = 'master' ]; then
-        wget https://fileserver-1253732882.cos.ap-chongqing.myqcloud.com/kubectl/kubectl-${KUBERNETES_VERSION}-0.x86_64.rpm
-        yum install -y kubectl-${KUBERNETES_VERSION}-0.x86_64.rpm
+        if [ "${ROLE}" = 'master' ]; then
+            wget https://fileserver-1253732882.cos.ap-chongqing.myqcloud.com/kubectl/kubectl_${KUBERNETES_VERSION}-00_amd64.deb
+            dpkg -i kubectl_${KUBERNETES_VERSION}-00_amd64.deb
+        fi
+        dpkg -i kubernetes-cni_0.6.0-00_amd64.deb kubelet_${KUBERNETES_VERSION}-00_amd64.deb
+        systemctl enable kubelet.service
+    elif [[ ${CentOS##*=} =~ "7" ]]; then
+        yum install -y wget
+        wget https://fileserver-1253732882.cos.ap-chongqing.myqcloud.com/kubernetes-cni/kubernetes-cni-0.6.0-0.x86_64.rpm
+        wget https://fileserver-1253732882.cos.ap-chongqing.myqcloud.com/kubelet/kubelet-${KUBERNETES_VERSION}-0.x86_64.rpm
+
+        if [ "${ROLE}" = 'master' ]; then
+            wget https://fileserver-1253732882.cos.ap-chongqing.myqcloud.com/kubectl/kubectl-${KUBERNETES_VERSION}-0.x86_64.rpm
+            yum install -y kubectl-${KUBERNETES_VERSION}-0.x86_64.rpm
+        fi
+        yum install -y kubernetes-cni-0.6.0-0.x86_64.rpm kubelet-${KUBERNETES_VERSION}-0.x86_64.rpm
+        systemctl enable kubelet.service
+    else
+        echo "The current operating system version is not supported."
     fi
-    yum install -y kubernetes-cni-0.6.0-0.x86_64.rpm kubelet-${KUBERNETES_VERSION}-0.x86_64.rpm
-    systemctl enable kubelet.service
 }
 
 PS3='Select kubernetes version: '
